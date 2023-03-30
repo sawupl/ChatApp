520f8a12d4c007afd624ba416e2c83134b9ab819
vbifarik <mtopv@mail.ru>
2023-03-30 21:08:51 +0400

    Надо добавить потоки

--- app/src/main/java/com/example/repository/screens/registrarion/RegistrationFragment.java
+++ app/src/main/java/com/example/repository/screens/registrarion/RegistrationFragment.java
@@ -2,7 +2,6 @@ package com.example.repository.screens.registrarion;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -12,18 +11,11 @@ import android.view.ViewGroup;
 
 import com.example.repository.R;
 import com.example.repository.databinding.FragmentRegistrationBinding;
-import com.example.repository.models.Chat;
-import com.example.repository.models.Message;
 import com.example.repository.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
 
 public class RegistrationFragment extends Fragment {
     private FragmentRegistrationBinding binding;
@@ -52,34 +44,38 @@ public class RegistrationFragment extends Fragment {
         return binding.getRoot();
     }
     private void registration(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(requireActivity(), task -> {
-                    if (task.isSuccessful()) {
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        writeNewUser(binding.login.getText().toString(),binding.name.getText().toString(), user.getUid());
-                        System.out.println("user created");
-//                        Navigation.findNavController(getView()).navigate(R.id.action_registrationFragment_to_chatFragment);
-                    } else {
-                        System.out.println("user didn't create");
-                    }
-                });
+        boolean f = checkLogin(binding.login.getText().toString());
+        if (f){
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(requireActivity(), task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            writeNewUser(binding.login.getText().toString(),binding.name.getText().toString(), user.getUid());
+                            System.out.println("user created");
+                            Navigation.findNavController(getView()).navigate(R.id.action_registrationFragment_to_chatFragment);
+                        }
+                        else {
+                            System.out.println("user didn't create");
+                        }
+                    });
+        }
+        else{
+
+        }
     }
     private void writeNewUser(String login,String name,String userId) {
-        checkLogin(login);
-//        User user = new User(name, login);
-
-//        mDatabase.child("users").child(userId).setValue(user);
+        User user = new User(name, login);
+        mDatabase.child("users").child(userId).setValue(user);
     }
 
     private Boolean checkLogin(String login) {
-        mDatabase.child("users").orderByChild("login").equalTo(login).limitToFirst(1).get().addOnCompleteListener(dataSnapshot -> {
+        return mDatabase.child("users").orderByChild("login").equalTo(login).limitToFirst(1).get().getResult().getValue() == null;
+        /*mDatabase.child("users").orderByChild("login").equalTo(login).limitToFirst(1).get().addOnCompleteListener(dataSnapshot -> {
             System.out.println(dataSnapshot.getResult().getKey());
             for (DataSnapshot data: dataSnapshot.getResult().getChildren()) {
                 System.out.println(data.getKey());
             }
-        });
-
-
+        });*/
 //                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
 //                    String login_search = dataSnapshot.getValue(String.class);
 //                    if (login_search.equals(login)) {
@@ -95,8 +91,5 @@ public class RegistrationFragment extends Fragment {
 //                    }
 //                }
 //            }
-
-
-        return false;
     }
 }
